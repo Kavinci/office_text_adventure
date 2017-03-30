49132a4099b8694cd0a6bc2d30dd521abad49af8
Kent <utterback.kent.a@gmail.com>
2017-03-30 14:58:25 -0400

    input filtering in progress

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -11,7 +11,6 @@ public class TextController : MonoBehaviour {
 
     string textConsole;
     string textInput;
-    bool begin;
     string currentTime;
     int currentMin;
     int currentHour;
@@ -20,10 +19,10 @@ public class TextController : MonoBehaviour {
     void Start () {
         consoleBox.text = "LUNCH BREAK IS OVER. LET'S BE HONEST, NO ONE REALLY WORKS AROUND HERE. " +
             "TRY TO WASTE TIME UNTIL THE END OF THE DAY. DON'T GET CAUGHT SLACKING OFF BY YOUR BOSS!\n\n" 
-            + "PRESS ENTER TO START";
+            + "TYPE START TO BEGIN.";
         inputBox.text = ">";
 
-        begin = true;
+        
 
         textConsole = "";
         textInput = ">";
@@ -182,36 +181,47 @@ public class TextController : MonoBehaviour {
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            checkInput(textInput);
+            filterInput(textInput);
             textInput = ">";
             inputBox.text = ">";
         }
     }
 
-    // String comparison for inputs
-    void checkInput(string input)
+    // filters out the action and target keywords
+    void filterInput(string input)
     {
-        if (begin)
+        string inputLo = input.ToLower();
+        string[] filter = inputLo.Split('>',' ');
+        string action = filter[1];
+        string target;
+        string currentLoc = "cubical";
+        int j = 0;
+
+        foreach(string s in filter)
         {
-            setTime(0);
-            textConsole = "YOU FIND YOURSELF IN YOUR CUBICAL AFTER LUNCH WITH NOTHING TO DO AND MR. BOSS WON'T LET YOU LEAVE EARLY. " +
-                "IN YOUR CUBE THERE IS A DESK IN FRONT OF YOU TO THE WEST. " +
-                "TO THE EAST THERE IS A FILING CABINET. " +
-                "TO THE SOUTH IS THE ONLY OPENING AND JUST A WALL TO THE NORTH.\n" + currentTime + "\n";
+            if(s == "")
+            {
 
-            consoleBox.text = textConsole;
+            }
+        }
 
-            begin = false;
+        if (filter.Length > 2)
+        {
+            target = filter[2];
         }
         else
         {
-            textConsole = textConsole + textInput + "\n";
-            consoleBox.text = textConsole;
+            target = "";
         }
+        
+        if (currentLoc == "cubical")
+        {
+            cubical(action, target, input);
+        }       
     }
 
     // Sets the clock for the game by adding minutes
-    void setTime(int minute)
+    internal void setTime(int minute)
     {
         currentMin = currentMin + minute;
 
@@ -229,18 +239,119 @@ public class TextController : MonoBehaviour {
         {
             currentTime = currentHour.ToString() + ":" + currentMin.ToString() + " PM";
         };
+        
+    }
+    #region room functions
+    enum rooms {cubical, cubeFarm, h1, h2, h3, h4, bathroom, landing, kitchen, serverRoom, confRoom, bossOffice, elevator};
+    enum people {lexie, ed, boss};
+    enum stuff {computer, cup, cabinet, coffeePot, fridge, vending, whiteboard, toilet, sink };
+    enum bossPath { h3, h2, h1, cubeFarm, landing, h4};
+    int compCount;
+    int fileCount;
+    int edCount;
+    int lexCount;
+    int coffeeCount;
+    int printCount;
+    int pooCount;
 
+    void cubical(string action, string target, string input)
+    {
+        
+        if(action == "help" && target == "")
+        {
+            consoleBox.text = "HELP STATEMENT GOES HERE\n" + currentTime + "\n";
+        }
+        else if (action == "start" && target == "")
+        {
+            setTime(0);
+            textConsole = "YOU FIND YOURSELF IN YOUR CUBICAL AFTER LUNCH WITH NOTHING TO DO AND MR. BOSS WON'T LET YOU LEAVE EARLY. " +
+                "IN YOUR CUBE THERE IS A DESK IN FRONT OF YOU TO THE WEST. " +
+                "TO THE EAST THERE IS A FILING CABINET. " +
+                "TO THE SOUTH IS THE ONLY OPENING AND JUST A WALL TO THE NORTH.\n" + currentTime + "\n\n" + textInput;
+
+            consoleBox.text = textConsole;
+        }
+        else
+        {
+            error(101, input);
+        }
+    }
+
+    void cubeFarm(string action, string target, string input)
+    {
+
+    }
+
+    void h1(string action, string target, string input)
+    {
+
+    }
+
+    void h2(string action, string target, string input)
+    {
+
+    }
+
+    void h3(string action, string target, string input)
+    {
+
+    }
+
+    void h4(string action, string target, string input)
+    {
+
+    }
+
+    void bathroom(string action, string target, string input)
+    {
+
+    }
+
+    void landing(string action, string target, string input)
+    {
+
+    }
+
+    void kitchen(string action, string target, string input)
+    {
+
+    }
+
+    void serverRoom(string action, string target, string input)
+    {
+
+    }
+
+    void confRoom(string action, string target, string input)
+    {
+
+    }
+
+    void bossOffice(string action, string target, string input)
+    {
+
+    }
+
+    void elevator(string action, string target, string input)
+    {
         if(currentHour >= 5)
         {
-            // Set the game over and clock out
+
+        }
+    }
+
+    #endregion
+    // Error codes to send out to the UI console
+    void error(int code, string input)
+    {
+        if(code == 101)
+        {
+            consoleBox.text = input + "?\n" + "I don't know how to do that. Press enter and try again.";
         }
         
     }
-
-    // Error codes to send out to the UI console
-    void error(int code)
-    {
-        consoleBox.text = "An Error was detected";
-    }
 }
+
+
+   
 
